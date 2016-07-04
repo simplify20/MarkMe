cc438ff6fa4034b6b6b915d13f13716d4dc2efcf
jianjun.yang@ymm56.com <jianjun.yang@ymm56.com>
2016-07-04 14:11:58 +0800

    fix bug

--- batch/mark.bat
+++ batch/mark.bat
@@ -1,4 +1,4 @@
-::@ECHO OFF
+@ECHO OFF
 SETLOCAL
 SET _option=%1
 ::Remove Quotes from a string use ~
@@ -11,7 +11,7 @@ SET inner_path=%USERPROFILE%\Documents\%mark_dir%\%inner_dir_name%
 REM
 SET marked_file=%USERPROFILE%\Documents\%mark_dir%\%marked_file_name%.md
 SET marked_file_backup=%inner_path%\%marked_file_name%.back
-::This two file will be removed after execution
+::This file will be removed after execution
 SET content_temp_file=%inner_path%\marked_content.tmp
 IF NOT EXIST %USERPROFILE%\Documents\%mark_dir% (
 	MD %USERPROFILE%\Documents\%mark_dir%
@@ -29,22 +29,24 @@ IF /I "%_option%" == "-s" (
 		ECHO Error:no url description
 		GOTO End
 	)
+	REM Format showing style here
+	SET format_date=###%DATE%
+	SET format_title=##Marked Contents
 	REM Query newest date and save to local variable
 	SETLOCAL ENABLEDELAYEDEXPANSION 
 	IF EXIST %marked_file% (
-		FOR /F  "tokens=1 delims=" %%i IN (%marked_file%) DO (
+		FOR /F  "tokens=1 delims=¦" %%i IN (%marked_file%) DO (
 			SET _line=%%i
-			SET sub_line=!_line:~0,7!
 			IF [!got_date!] == [1] (
 				::Create content file dynamicly
 				IF NOT EXIST %content_temp_file% (
-					TYPE nul > %content_temp_file%
+					TYPE nul> %content_temp_file%
 				) 
-				ECHO !_line! >> %content_temp_file%	
+				ECHO !_line!>> %content_temp_file%
 			)
 			IF [!got_date!] == [] (
-				IF !sub_line! == ###2016 (
-					SET newest_date=!_line!
+				IF "!_line!" == "!format_date!" (
+					SET newest_date=!_line:~3!
 				    SET /A got_date=1
 				) 
 			)
@@ -55,47 +57,55 @@ IF /I "%_option%" == "-s" (
 	:Next
 	REM backup marked file
 	IF EXIST %marked_file% (
-		TYPE %marked_file% > %marked_file_backup%
+		TYPE %marked_file%> %marked_file_backup%
 	)
-	ECHO ##Marked Contents > %marked_file%
-	ECHO ###%DATE:~0,10% >> %marked_file%
+	ECHO !format_title!> %marked_file%
+	REM Do not modify here
+	ECHO !format_date!>> %marked_file%
 	REM Use temp file to save clipboard content
 	TYPE nul > temp.txt
 	GetClip /txt >> temp.txt
-	FOR /F "tokens=1 delims=" %%i IN (temp.txt) DO (
+	FOR /F "tokens=1 delims=¦" %%i IN (temp.txt) DO (
 		SET clip_line=%%i
-		GOTO AddUrl
+		GOTO AddContent
 	)
-	:AddUrl
+	:AddContent
 	REM Add new content
 	REM Format content showing style here
+	REM
+	REM
+	REM
 	IF [%_bold%] ==[] (
-		ECHO *%_desc:~1,-1%:* >> %marked_file%
+		ECHO *%_desc:~1,-1%:*>> %marked_file%
 	) ELSE (
-		ECHO ######*%_desc:~1,-1%:* >> %marked_file%
+		ECHO ######*%_desc:~1,-1%:*>> %marked_file%
 		REM Tricks
 		SET _quto=">"
-		ECHO !_quto:~1,-1! >> %marked_file%
+		ECHO !_quto:~1,-1!>> %marked_file%
 	)
 	TYPE temp.txt >> %marked_file%
 	IF /I "%_bold%" =="-b" (
-		ECHO. >> %marked_file%
+		ECHO.>> %marked_file%
 	)
-	ECHO. >> %marked_file%
-	SET current_date=%DATE:~0,10%
+	REM
+	REM
+	REM
+	ECHO.>> %marked_file%
+	SET current_date=%DATE%
 	IF [!newest_date!] NEQ [] (
-		IF !newest_date:~-10! NEQ !current_date! (
+		IF "!newest_date!" NEQ "!current_date!" (
 			REM Copy date to mark file
-			ECHO !newest_date! >> %marked_file%
+			ECHO !newest_date!>> %marked_file%
 		)
 		::ECHO "Merge"
 		REM Merge content to marked_file
-		TYPE %content_temp_file% >> %marked_file%
+		TYPE %content_temp_file%
+		TYPE %content_temp_file%>> %marked_file%
 	) ELSE (
 		REM do nothing
 	)
 	IF EXIST %content_temp_file% (
-		DEL %content_temp_file%
+		REM DEL %content_temp_file%
 	)
 	IF %ERRORLEVEL% == 0  (
 		@ECHO Marked:!clip_line!
@@ -111,7 +121,7 @@ IF /I "%_option%" == "-s" (
 ) ELSE IF /I "%_option%" == "-r" (
 	ECHO Reset content...
 	IF EXIST %marked_file_backup% (
-		TYPE %marked_file_backup% > %marked_file%
+		TYPE %marked_file_backup%> %marked_file%
 	) ELSE (
 		ECHO Reset failed,backup file is not exist.
 	)
